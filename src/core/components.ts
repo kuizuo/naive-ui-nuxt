a12ae350fcace2d36ed574d08e35c9cb5982afb3
kuizuo <hi@kuizuo.cn>
2023-02-27 22:47:48 +0800

    fix: Cannot find module in development

--- src/core/components.ts
+++ src/core/components.ts
@@ -1,15 +1,15 @@
-import { addComponent } from "@nuxt/kit";
-import { libraryName } from "../config";
+import { addComponent } from "@nuxt/kit"
+import { libraryName } from "../config"
 import { ModuleOptions } from '../module'
 
 export function resolveComponents(config: ModuleOptions) {
-  const { components } = config;
+  const { components } = config
 
   components.forEach((item) => {
     addComponent({
       export: item,
       name: item,
       filePath: libraryName,
-    });
-  });
+    })
+  })
 }
--- src/core/options.ts
+++ src/core/options.ts
@@ -1,20 +1,16 @@
-import { useNuxt } from "@nuxt/kit";
+import { useNuxt } from "@nuxt/kit"
 
 export function resolveOptions() {
   const nuxt = useNuxt();
 
-  // nuxt.options.build.transpile.push(libraryName)
-  const dependencies =
-    process.env.NODE_ENV === "production"
+  nuxt.options.build.transpile.push(
+    ...(process.env.NODE_ENV === "production"
       ? ["naive-ui", "vueuc", "@css-render/vue3-ssr", "@juggle/resize-observer"]
-      : ["@juggle/resize-observer"];
+      : ["@juggle/resize-observer"]));
 
-  nuxt.options.build.transpile.push(...dependencies);
-
-  nuxt.options.vite.optimizeDeps = {
-    include:
-      process.env.NODE_ENV === "development"
-        ? ["naive-ui", "vueuc", "date-fns-tz/esm/formatInTimeZone"]
-        : [],
-  };
+  nuxt.options.vite.optimizeDeps?.include?.push(
+    ...(process.env.NODE_ENV === "development"
+      ? ["naive-ui", "vueuc", "date-fns-tz/esm/formatInTimeZone"]
+      : [])
+  )
 }
